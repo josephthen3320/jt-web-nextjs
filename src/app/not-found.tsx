421175934deb8aec644670b86f1e77897806df37
josephthen3320 <josephthenara@outlook.com>
2025-04-30 11:05:23 +0800

    update: 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,7 +11,7 @@ export default function NotFound() {
                 <div className="border border-gray-600 rounded-lg p-8 text-center backdrop-blur-sm">
                     <h1 className="text-2xl font-medium mb-3">This content is not available in your country</h1>
                     <p className="text-gray-600">
-                        The page or resource you're trying to access is currently not available.
+                        The page or resource you&#39;re trying to access is currently not available.
                     </p>
                 </div>
             </div>
